fb800aee3e3e2973286d4343f0efaa7d11c01d69
Jose <jchongac@fiec.espol.edu.ec>
2024-08-01 14:24:26 -0500

    Modifying r-lsp settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -108,7 +108,16 @@ return {
             marksman = {},
             cssls = { filetypes = { 'css', 'scss' }},
             tailwindcss = {},
-            r_language_server = {},
+            r_language_server = {
+                r = {
+                    lsp = {
+                        diagnostics = {
+                            spacesAroundInfixOperators = false,
+                            lineWidth = nil,
+                        }
+                    }
+                }
+            },
             html = { filetypes = { 'html', 'twig', 'hbs' } },
             lua_ls = {
                 Lua = {
