08704e2a28d1e5310ed01db52d8f4833d50697c5
Jose <jchongac@fiec.espol.edu.ec>
2024-09-19 13:36:16 -0500

    Added groups to which key

--- lua/custom/which-key.lua
+++ lua/custom/which-key.lua
@@ -5,13 +5,17 @@ return {
     config = function ()
         require('which-key').add {
             { '<leader>c', group = '[C]ode' },
-            { '<leader>d', group = '[D]ocument' },
+            { '<leader>d', group = '[D]ebug [D]ocument' },
             { '<leader>g', group = '[G]it' },
-            { '<leader>r', group = '[R]ename, [R]eplace' },
+            { '<leader>r', group = '[R]ename [R]eplace' },
             { '<leader>h', group = 'Git [H]unk' },
             { '<leader>s', group = '[S]earch' },
             { '<leader>w', group = '[W]orkspace' },
+            { '<leader>b', group = '[B]uffer [B]reakpoint' },
             { '<leader>f', group = '[F]ile' },
+            { '<leader>l', group = '[L]sp' },
+            { '<leader>i', group = '[I]nlay' },
+            { '<leader>v', group = '[V]ariable' },
             { 's', group = '[S]urround'},
         }
     end
