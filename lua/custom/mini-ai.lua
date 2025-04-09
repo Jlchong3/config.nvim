2ae8499e961fea6ec310e8b3848867e05eb6776d
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2025-04-09 18:16:26 -0400

    Add mini-ai text objects

--- lua/custom/mini-ai.lua
+++ lua/custom/mini-ai.lua
@@ -6,8 +6,8 @@ return {
         MiniAi.setup{
             custom_textobjects = {
                 o = MiniAi.gen_spec.treesitter{
-                    a = {'@conditional.outer', '@loop.outer'},
-                    i = {'@conditional.inner', '@loop.inner'}
+                    a = {'@block.outer', '@conditional.outer', '@loop.outer'},
+                    i = {'@block.inner','@conditional.inner', '@loop.inner'}
                 },
 
                 f = MiniAi.gen_spec.treesitter{
@@ -20,13 +20,24 @@ return {
                     i = {'@class.inner'},
                 },
 
+                t = { '<([%p%w]-)%f[^<%w][^<>]->.-</%1>', '^<.->().*()</[^/]->$' },
+
+                d = { "%f[%d]%d+" },
+
                 b = { { '%b()' }, '^.().*().$' },
 
                 B = { { '%b{}' }, '^.().*().$' },
 
-                a = MiniAi.gen_spec.function_call(),
+                e = {
+                    { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+                    "^().*()$",
+                },
 
                 i = MiniAi.gen_spec.argument(),
+
+                u = MiniAi.gen_spec.function_call(),
+
+                U = MiniAi.gen_spec.function_call({ name_pattern = "[%w_]" }),
             },
 
             n_lines = 500
