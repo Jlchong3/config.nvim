d2fade21951b4e4fead482fc900ab045b94ba870
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2025-04-27 22:20:26 -0400

    Add yuck TS

--- lua/custom/treesitter.lua
+++ lua/custom/treesitter.lua
@@ -26,6 +26,7 @@ return {
                 'javascript',
                 'java',
                 'zig',
+                'yuck',
                 'typescript',
                 'vimdoc',
                 'vim',
