eebf99dd063e034d8b2f7a49e00127a665c717d7
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2025-08-26 18:51:49 -0500

    Simplify treesitter start

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -12,63 +12,41 @@ vim.pack.add {
 }
 
 local parsers = {
-    c = { "*.c", "*.h" },
-    cpp = { "*.cpp", "*.hpp", "*.cc", "*.cxx", "*.hh", "*.hxx" },
-    go = { "*.go" },
-    lua = { "*.lua" },
-    python = { "*.py", "*.pyw" },
-    rust = { "*.rs" },
-    tsx = { "*.tsx" },
-    html = { "*.html", "htm" },
-    css = { "*.css" },
-    glsl = { "*.glsl", "*.vert", "*.frag", "*.geom" },
-    json = { "*.json" },
-    javascript = { "*.js", "*.mjs", "*.cjs" },
-    java = { "*.java" },
-    zig = { "*.zig" },
-    yuck = { "*.yuck" },
-    typescript = { "*.ts" },
-    nix = { "*.nix" },
-    vimdoc = { "*.txt" },  -- Neovim help docs
-    vim = { "*.vim" },
-    bash = { "*.sh", "*.bash" },
-    sql = { "*.sql" },
-    markdown = { "*.md", "*.markdown" },
-    markdown_inline = { "*.md", "*.markdown" }, -- same extensions
-    yaml = { "*.yaml", "*.yml" },
-    odin = { "*.odin" },
+    'c',
+    'cpp',
+    'go',
+    'lua',
+    'python',
+    'rust',
+    'tsx',
+    'html',
+    'css',
+    'glsl',
+    'json',
+    'javascript',
+    'java',
+    'zig',
+    'yuck',
+    'typescript',
+    'nix',
+    'vimdoc',
+    'vim',
+    'bash',
+    'sql',
+    'markdown',
+    'markdown_inline',
+    'yaml',
+    'odin',
 }
 
 require('nvim-treesitter').install(vim.tbl_keys(parsers));
 require('treesitter-context').setup { enable = true, }
 require('nvim-ts-autotag').setup {}
 
-
-local all_exts = {}
-for _, exts in pairs(parsers) do
-  for _, ext in ipairs(exts) do
-    table.insert(all_exts, ext)
-  end
-end
-
 vim.api.nvim_create_augroup('TSGroup', {})
 vim.api.nvim_create_autocmd('BufEnter', {
     group = 'TSGroup',
-    callback = function(e)
-        local ft = vim.bo[e.buf].filetype
-
-        if parsers[ft] then
-            pcall(vim.treesitter.start, e.buf)
-            return
-        end
-
-        -- fallback: check filename against patterns
-        local name = vim.api.nvim_buf_get_name(e.buf)
-        for _, pattern in ipairs(all_exts) do
-            if name:match(pattern:gsub("%*", ".*")) then
-                pcall(vim.treesitter.start, e.buf)
-                return
-            end
-        end
+    callback = function()
+        pcall(vim.treesitter.start)
     end
 })
