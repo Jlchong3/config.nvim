2eefa65743c640c1b8c5ace430ff5a0c608fa3e2
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2025-09-08 23:45:41 -0500

    Move to main branch for my plugin

--- plugin/cabinet.lua
+++ plugin/cabinet.lua
@@ -1,8 +1,5 @@
 vim.pack.add {
-    {
-        src = 'https://github.com/Jlchong3/cabinet.nvim',
-        version = 'refactor'
-    }
+    'https://github.com/Jlchong3/cabinet.nvim',
 }
 
 local cabinet = require('cabinet').setup {}
