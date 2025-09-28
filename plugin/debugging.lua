85413cd05b82fdcf2c3cea9e63eec895b4dac9a6
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2025-09-28 03:16:58 -0500

    fix dependency error

--- plugin/debugging.lua
+++ plugin/debugging.lua
@@ -1,24 +1,24 @@
-vim.pack.add ({
-  'https://github.com/igorlfs/nvim-dap-view'
-}, { load = true })
 vim.pack.add {
     'https://github.com/mfussenegger/nvim-dap',
     'https://github.com/leoluz/nvim-dap-go',
     'https://github.com/mfussenegger/nvim-dap-python',
 }
+vim.pack.add ({
+  'https://github.com/igorlfs/nvim-dap-view',
+}, { load = true })
 
 vim.keymap.set('n', '<F5>', function ()
-  vim.keymap.del('n', '<F5>')
 
+  vim.keymap.del('n', '<F5>')
   require('debugging')
 
   require('dap').continue()
 end)
 
 vim.keymap.set('n', '<leader>bp', function ()
-  vim.keymap.del('n', '<leader>bp')
-
   require('debugging')
+
+  vim.keymap.del('n', '<leader>bp')
 
   require('dap').toggle_breakpoint()
 end)
