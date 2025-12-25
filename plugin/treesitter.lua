24d570006f253fe68077e12afd6f78c87337a0d1
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2025-12-24 19:04:40 -0500

    Keeping nvim-treesitter-textobjects version

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,6 +1,9 @@
 vim.pack.add ({
     'https://github.com/nvim-treesitter/nvim-treesitter',
-    'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
+    {
+        src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
+        version = 'main'
+    },
     'https://github.com/windwp/nvim-ts-autotag',
     'https://github.com/nvim-treesitter/nvim-treesitter-context',
 }, { load = true } )
