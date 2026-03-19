6ff7c680d8043132b704446e3b7226deebceb5de
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2026-03-19 00:34:47 -0500

    Change colorscheme

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -1,7 +1,5 @@
-vim.cmd.colorscheme 'miniautumn'
+vim.cmd.colorscheme 'retrobox'
 
-vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#7c7086" })
-vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#7c7086" })
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#f5c2e7", bold = true })
-vim.api.nvim_set_hl(0, "MiniStatuslineFilename", { bg ="#262029", bold = true})
-vim.api.nvim_set_hl(0, "MiniStatuslineInactive", { bg ="#262029" })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#7c6f64" })
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#7c6f64" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#CcBfB4", bold = true })
