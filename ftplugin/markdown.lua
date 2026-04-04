ba8e10a173c14ce568e9585b563c7220984ae3c5
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2026-04-04 02:10:26 -0500

    Fix: using nvim_set_option_value instead of vim.bo

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,6 +1,6 @@
-vim.bo.wrap = true
-vim.bo.list = true
-vim.bo.linebreak = true
+vim.api.nvim_set_option_value('wrap', true, { scope = 'local' })
+vim.api.nvim_set_option_value('list', false, { scope = 'local' })
+vim.api.nvim_set_option_value('linebreak', true, { scope = 'local' })
 
 vim.keymap.set('n', '<leader>ch', function ()
     local current_line = vim.api.nvim_get_current_line();
--- ftplugin/typst.lua
+++ ftplugin/typst.lua
@@ -1,3 +1,3 @@
-vim.bo.wrap = true
-vim.bo.list = true
-vim.bo.linebreak = true
+vim.api.nvim_set_option_value('wrap', true, { scope = 'local' })
+vim.api.nvim_set_option_value('list', false, { scope = 'local' })
+vim.api.nvim_set_option_value('linebreak', true, { scope = 'local' })
