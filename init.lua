a9972984960443819cd6c48386da8d89e915bfe2
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2026-04-07 00:22:20 -0500

    Disable unused builtin plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,21 @@
+local builtins = {
+    'gzip',
+    'zip',
+    'zipPlugin',
+    'tar',
+    'tarPlugin',
+    'getscript',
+    'matchit',
+    'getscriptPlugin',
+    'vimball',
+    'vimballPlugin',
+    'netrw',
+}
+
+for _, plugin in ipairs(builtins) do
+    vim.g['loaded_' .. plugin] = 1
+end
+
 require('options')
 require('remaps')
 require('autocmd')
