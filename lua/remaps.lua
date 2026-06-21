c2e5a661c85a359c42302afc786ae59df858384f
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2026-06-21 10:34:11 -0500

    Move markup function to remaps to support orgmode

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -101,30 +101,115 @@ vim.keymap.set('t', '<Esc><Esc>', [[<C-\><C-n>]])
 -- Make
 vim.keymap.set('n', '<leader>m', ':make ', { desc = '[M]ake' })
 
--- Runner for class
-local ClassGroup = vim.api.nvim_create_augroup('Class', { clear = true })
-
+local MarkupGroup = require('augroups').MarkupGroup
 vim.api.nvim_create_autocmd('BufEnter', {
-    group = ClassGroup,
-    pattern = { "clase.py" },
+    group = MarkupGroup,
+    pattern = { '*.org', '*.md' },
+    callback = function()
+        vim.keymap.set('n', '<leader>ch', function ()
+            local current_line = vim.api.nvim_get_current_line();
+
+            if current_line:match('^%s*$') then
+                return
+            end
+
+            local non_blank = current_line:match('^%s*'):len() + 1
+            local sub_string = current_line:sub(non_blank, non_blank + 4)
+            local row, _ = unpack(vim.api.nvim_win_get_cursor(0));
+            if sub_string == '- [ ]' then
+                vim.api.nvim_buf_set_text(0, row - 1, non_blank + 2, row - 1, non_blank + 3, {'x'});
+            elseif sub_string == '- [x]' then
+                vim.api.nvim_buf_set_text(0, row - 1, non_blank + 2, row - 1, non_blank + 3, {' '});
+            else
+                vim.api.nvim_buf_set_text(0, row - 1, non_blank - 1, row - 1, non_blank - 1, {'- [ ] '});
+            end
+        end, { buffer = true, desc = '[CH]eck box' })
+
+        vim.keymap.set('v', '<leader>ch', function ()
+            vim.api.nvim_feedkeys('\027', 'xt', false)
+            local start_line, _ = unpack(vim.api.nvim_buf_get_mark(0, '<'))
+            local end_line, _ = unpack(vim.api.nvim_buf_get_mark(0, '>'))
+            local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
+            local non_checkbox_lines = {}
+            local unchecked_checkbox_lines = {}
+
+            for i = 1, #lines do
+                local non_blank = lines[i]:match('^%s*'):len() + 1
+                local sub_string = lines[i]:sub(non_blank, non_blank + 4)
+                local non_checkbox = sub_string ~= '- [ ]' and sub_string ~= '- [x]'
+                local is_empty = sub_string:match('^%s*$')
+                if non_checkbox and not is_empty then
+                    non_checkbox_lines[i] = non_blank
+                elseif sub_string == '- [ ]' then
+                    unchecked_checkbox_lines[i] = non_blank
+                end
+            end
+
+            if vim.tbl_count(non_checkbox_lines)~= 0 then
+                for ind, non_blank in pairs(non_checkbox_lines) do
+                    local row = start_line + ind - 1
+                    vim.api.nvim_buf_set_text(0, row - 1, non_blank - 1, row - 1, non_blank - 1, {'- [ ] '});
+                end
+            elseif vim.tbl_count(unchecked_checkbox_lines) == 0 then
+                for i = 1, #lines do
+                    lines[i] = lines[i]:gsub('- %[x%]', '- [ ]', 1)
+                end
+                vim.api.nvim_buf_set_lines(0, start_line - 1, end_line, false, lines)
+            else
+                for ind, non_blank in pairs(unchecked_checkbox_lines) do
+                    local row = start_line + ind - 1
+                    vim.api.nvim_buf_set_text(0, row - 1, non_blank + 2, row - 1, non_blank + 3, {'x'});
+                end
+            end
+        end, { buffer = true, desc = '[CH]eck box' })
+    end
+})
+
+-- Runner for class
+local TempGroup = vim.api.nvim_create_augroup('Temp', { clear = true })
+-- Make this into a small program or plugin
+vim.api.nvim_create_autocmd('BufEnter', {
+    group = TempGroup,
+    pattern = { "*.py" },
     callback = function(e)
         vim.keymap.set(
             'n',
             '<C-p>',
             function ()
                 local win_height = vim.api.nvim_win_get_height(0)
-                local runner_height = win_height / 3
+                local runner_height = math.floor(win_height / 3)
                 local buf = vim.api.nvim_create_buf(false, true)
-                vim.api.nvim_open_win(buf, true, {
+                local win = vim.api.nvim_open_win(buf, true, {
                     split = "below",
                     win = 0,
                     height = runner_height
                 })
                 local task = "python " .. e.file
-                vim.fn.jobstart(task, { term = true })
+                vim.fn.jobstart(task, {
+                    term = true,
+                    on_exit = function ()
+                        if vim.api.nvim_get_current_buf() == buf then
+                            vim.cmd('stopinsert')
+                        end
+                    end
+                })
                 vim.api.nvim_feedkeys('i', 'n', false)
+
+                vim.keymap.set('n', 'Q', function()
+                    if vim.api.nvim_win_is_valid(win) then
+                        vim.api.nvim_win_close(win, true)
+                    end
+                end)
+
+                vim.keymap.set('n', 'q', function()
+                    if vim.api.nvim_win_is_valid(win) then
+                        vim.api.nvim_win_close(win, true)
+                    end
+                end, { buf = buf })
             end,
             { buffer = e.buf }
         )
     end
 })
+
+
